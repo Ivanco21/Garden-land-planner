fd8faf53003146588b53472900ba4bb1e12f3546
FoggyFinder <FoggyFinder@yandex.ua>
2020-01-12 16:15:20 +0200

    move options to vm

--- VM/MainVM.cs
+++ VM/MainVM.cs
@@ -3,11 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using WpfClientForm.Model;
 
 namespace WpfClientForm.VM
 {
     public class MainVM : ViewModelBase
     {
+        public MainVM()
+        {
+            Cultures = Enum.GetValues(typeof(Culture)).Cast<Culture>().ToArray();
 
+            CreateCommand = new RelayCommand(p => Console.WriteLine($"Param = {p}"));
+        }
+
+
+        public ICommand CreateCommand { get; }
+        public Culture[] Cultures { get; }
     }
 }
